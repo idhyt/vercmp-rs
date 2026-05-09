a4954d09f5ab44a6c070667ec6d5b710083fc575
idhyt <gouml@chinatelecom.cn>
2026-05-10 00:04:30 +0800

    :white_check_mark: Pass

---
+++ rpm-version/src/main.c
@@ -0,0 +1,38 @@
+/* gcc main.c rpmvercmp.c -o main
+ * > ./main 1.2.1_alpha 1.2.1_beta
+ */
+#include "version.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("用法: %s <版本号1> <版本号2>\n", argv[0]);
+    printf("示例: %s 1.2.3a 1.2.3_beta1-r0\n", argv[0]);
+    return 1;
+  }
+
+  const char *a = argv[1];
+  const char *b = argv[2];
+
+  int cmp = rpm_version_compare(a, b);
+
+  const char *symbol;
+  switch (cmp) {
+  case RPM_VERSION_EQUAL:
+    symbol = "==";
+    break;
+  case RPM_VERSION_LESS:
+    symbol = "<";
+    break;
+  case RPM_VERSION_GREATER:
+    symbol = ">";
+    break;
+  default:
+    symbol = "?";
+    break;
+  }
+
+  printf("'%s' %s '%s'\n", a, symbol, b);
+
+  return 0;
+}
