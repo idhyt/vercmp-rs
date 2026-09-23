3f7e360aeb29ca7b1a5a80b212aa2fb189ec74d7
idhyt <gouml@chinatelecom.cn>
2026-09-23 15:20:51 +0800

    🔥 Remove unused scripts

--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-cargo test --workspace -- --nocapture
-# cargo test -p apk-version -- test_apk_version_compare --show-output
-# cargo test -p debian-version -- test_debian_version_compare --show-output
-# cargo test -p rpm-version -- test_rpm_version_compare --show-output
-# cargo test -p semantic-version -- test_semver_compare --show-output
-# cargo test -p alpm-version -- test_alpm_version_compare --show-output
